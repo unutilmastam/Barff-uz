f7115c5f7ad1676323138fbd0ac4dcffcf1ac7a2
BAKHRULLO <nbcapital.uz@gmail.com>
2026-08-27 15:06:37 +0300

    Delete app directory

--- app/news/[slug]/not-found.tsx
+++
@@ -1,9 +0,0 @@
-'use client';
-
-import { NotFoundState } from '@/components/ui/NotFoundState';
-import { useLocale } from '@/components/providers/LocaleProvider';
-
-export default function ArticleNotFound() {
-  const { t } = useLocale();
-  return <NotFoundState title={t.article.notFound} href="/news" label={t.article.back} />;
-}
